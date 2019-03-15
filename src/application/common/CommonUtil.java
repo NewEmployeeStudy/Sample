4326c2eb1bac61c2d5a98ea2011aa8e315afdfd0
hiroshi1118 <hiroshi19721118.goodman@gmail.com>
2019-03-16 00:26:51 +0900

    共通ユーティリティクラスを追加 メインメニューコントロールクラスを更新

---
+++ src/application/common/CommonUtil.java
@@ -0,0 +1,31 @@
+package application.common;
+
+/***
+ * 共通ユーティリティクラス
+ * @author H.Isogai
+ *
+ */
+public class CommonUtil {
+
+	/***
+	 * 共通ユーティリティクラスシングルトンインスタンス
+	 */
+	private CommonUtil() {
+	}
+
+	/***
+	 * 実行中のクラス名を取得
+	 * @return
+	 */
+	public static String getClassName() {
+		return Thread.currentThread().getStackTrace()[2].getClassName();
+	}
+
+	/***
+	 * 実行中のメソッド名を取得
+	 * @return
+	 */
+	public static String getMethodName() {
+		return Thread.currentThread().getStackTrace()[2].getMethodName();
+	}
+}
--- src/application/control/Vmt001Controller.java
+++ src/application/control/Vmt001Controller.java
@@ -1,13 +1,17 @@
-/***
- * メインメニューコントローラー
- */
 package application.control;
 
+import application.Main;
+import application.common.CommonUtil;
 import application.common.LogUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+/***
+ * メインメニューコントローラ
+ *
+ * @author H.Isogai
+ */
 public class Vmt001Controller {
 
 	// ログユーティリティー
@@ -31,7 +35,8 @@ public class Vmt001Controller {
 	 */
 	@FXML
 	public void onActionVms001(ActionEvent e) {
-
+		log.log(String.format("%s.%s", CommonUtil.getClassName(), CommonUtil.getMethodName()));
+		new Main().changeView("VMS001.fxml");
 	}
 
 	/***
@@ -39,6 +44,25 @@ public class Vmt001Controller {
 	 */
 	@FXML
 	public void onActionVmd001(ActionEvent e) {
+		log.log(String.format("%s.%s", CommonUtil.getClassName(), CommonUtil.getMethodName()));
+		new Main().changeView("VMD001.fxml");
+	}
 
+	/***
+	 * メンテナンスボタン押下時の遷移
+	 */
+	@FXML
+	public void onActionVmm001(ActionEvent e) {
+		log.log(String.format("%s.%s", CommonUtil.getClassName(), CommonUtil.getMethodName()));
+		new Main().changeView("VMM001.fxml");
+	}
+
+	/***
+	 * データ取り込みボタン押下時の遷移
+	 */
+	@FXML
+	public void onACtionVmg001(ActionEvent e) {
+		log.log(String.format("%s.%s", CommonUtil.getClassName(), CommonUtil.getMethodName()));
+		new Main().changeView("VMG001.fxml");
 	}
 }
