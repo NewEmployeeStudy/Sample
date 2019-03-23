6b061fd1dd9c5e7cd99c3844abb937e73a152125
hiroshi1118 <hiroshi19721118.goodman@gmail.com>
2019-03-23 21:11:54 +0900

    fx:idとinitializeを変更

--- src/application/control/Vmd001Controller.java
+++ src/application/control/Vmd001Controller.java
@@ -1,6 +1,7 @@
 package application.control;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 
@@ -17,10 +18,12 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 /***
  * 詳細画面コントローラ
@@ -87,38 +90,15 @@ public class Vmd001Controller implements Initializable {
 	// 割当先等
 	@FXML
 	private TableColumn<TSecuritiesModel, String> tblColAllotment;
+	// 更新ボタン
+	@FXML
+	private Button btnRegist;
+	// フォーム
+	@FXML
+	private AnchorPane frmDetail;
 
 	// TableViewへ格納するためのオブジェクト
 	private ObservableList<TSecuritiesModel> olData;
-
-	@FXML
-	public void onLoadfrmDetail(ActionEvent e) {
-		log.log(String.format("%s.%s", CommonUtil.getClassName(), CommonUtil.getMethodName()));
-
-		try {
-			this.initialize();
-
-			String strStockCd = null;
-
-			/// 詳細情報DAO
-			// 会社情報
-			MCompanyModel mCompany = Vmd001Dao.getHeader(strStockCd);
-			txtCompanyName.setText(mCompany.getCompanyName());
-			txtStockCd.setText(mCompany.getStockCd());
-			txtFoundationDate.setText(mCompany.getFoundationDate());
-			// 上場市場名へ変換
-			txtListedMarket.setText(ConstUtil.eListMarketCd.valueOf(mCompany.getListedMarketCd()).getName());
-			TExchangeStockCorpnoModel tExchange = Vmd001Dao.getHeader(mCompany);
-			txtCorporateNo.setText(String.valueOf(tExchange.getCorporateNo()));
-
-			// 財務情報
-			for (TSecuritiesModel tSec : Vmd001Dao.getList(strStockCd)) {
-				olData.addAll(tSec);
-			}
-		} catch (Exception ex) {
-			log.log(Level.SEVERE, "エラーが発生しました。", ex);
-		}
-	}
 
 	/***
 	 * 戻るボタン押下処理
@@ -133,8 +113,9 @@ public class Vmd001Controller implements Initializable {
 
 	/***
 	 * 初期化
+	 * @throws SQLException
 	 */
-	public void initialize() {
+	public void initialize() throws SQLException {
 		log.log(String.format("%s.%s", CommonUtil.getClassName(), CommonUtil.getMethodName()));
 
 		/// 財務情報
@@ -158,6 +139,24 @@ public class Vmd001Controller implements Initializable {
 		tblColStockPrice.setCellValueFactory(new PropertyValueFactory<>("stockPrice"));
 		tblColEnterpriseValue.setCellValueFactory(new PropertyValueFactory<>("enterpriseValue"));
 		tblColAllotment.setCellValueFactory(new PropertyValueFactory<>("allotment"));
+
+		String strStockCd = null;
+
+		/// 詳細情報DAO
+		// 会社情報
+		MCompanyModel mCompany = Vmd001Dao.getHeader(strStockCd);
+		txtCompanyName.setText(mCompany.getCompanyName());
+		txtStockCd.setText(mCompany.getStockCd());
+		txtFoundationDate.setText(mCompany.getFoundationDate());
+		// 上場市場名へ変換
+		txtListedMarket.setText(ConstUtil.eListMarketCd.valueOf(mCompany.getListedMarketCd()).getName());
+		TExchangeStockCorpnoModel tExchange = Vmd001Dao.getHeader(mCompany);
+		txtCorporateNo.setText(String.valueOf(tExchange.getCorporateNo()));
+
+		// 財務情報
+		for (TSecuritiesModel tSec : Vmd001Dao.getList(strStockCd)) {
+			olData.addAll(tSec);
+		}
 	}
 
 	/***
@@ -165,6 +164,11 @@ public class Vmd001Controller implements Initializable {
 	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.initialize();
+		try {
+			this.initialize();
+		} catch (SQLException ex) {
+			log.log(Level.SEVERE, "エラーが発生しました。", ex);
+			ex.printStackTrace();
+		}
 	}
 }
